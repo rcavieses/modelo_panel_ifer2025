3e042c18e6b65547f9d848c27f233ef283ecf7dd
rcavieses <cavieses@uabcs.mx>
2025-11-05 18:54:20 -0700

    modified:   src/main.py

--- src/main.py
+++ src/main.py
@@ -5,7 +5,7 @@ Punto de entrada para ejecutar la aplicación con interfaz gráfica.
 
 import sys
 import os
-
+#hola
 # Agregar el directorio src al path para importaciones
 sys.path.insert(0, os.path.join(os.path.dirname(__file__)))
 
